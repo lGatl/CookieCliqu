da871f512795f8e214a59d8c1a26de21161418aa
lGatl <gat55@live.fr>
2016-12-28 15:34:21 +0100

    avec kc

--- script.js
+++ script.js
@@ -1,54 +1,91 @@
-var bouton=G("#bouton"), div=G("#affichage"),multip=G("#multip"),autocq=G("#autocq"),
-score=0,compteur=1,prix=50,auto=0, prixauto=200,timer;
-multip.innerHTML="Multiplicateur x"+compteur+ " prix"+prix;
-autocq.innerHTML="Autoclique vitesse x"+auto+ " prix"+prixauto;
+var bouton		= G("#bouton"),
+	div			= G("#affichage"),
+	multip		= G("#multip"),
+	autocq		= G("#autocq"),
+	score		= 0,
+	compteur	= 1,
+	prix			= 50,
+	auto		= 0,
+	prixauto	= 200,
+	kc			= 1,
+	timer;
+
+multip.innerHTML = "Multiplicateur x" + compteur + " prix" + prix;
+autocq.innerHTML = "Autoclique vitesse x" + auto + " prix" + prixauto;
 
 
 
-bouton.addEventListener('click', function(e){         /*Lorsque l'on clique sur le bouton*/
-    e.preventDefault();
-    cliq();
+bouton.addEventListener('click', function(e) { /*Lorsque l'on clique sur le bouton*/
+	e.preventDefault();
+	cliq();
 
 });
 
-multip.addEventListener('click', function(e){
+multip.addEventListener('click', function(e) {
 	e.preventDefault();
 	incrementer();
 });
 
-var cliq=function(){
-		score = score + compteur;
-		div.innerHTML=score;
-}
+var cliq = function() {
+	score = score + compteur;
+	div.innerHTML = score;
+};
 
-var incrementer = function(){
-	if(score>=prix){
-		score=score-prix;
+var incrementer = function() {
+	if (score >= prix) {
+		score = score - prix;
 		compteur++;
-		div.innerHTML=score;
-		prix=prix*2;
-		multip.innerHTML="Multiplicateur x"+compteur+ " prix"+prix;
+		div.innerHTML = score;
+		prix = prix * 2;
+		multip.innerHTML = "Multiplicateur x" + compteur + " prix" + prix;
 	}
 };
 
-autocq.addEventListener('click', function(e){
+autocq.addEventListener('click', function(e) {
 	e.preventDefault();
 	activetimer();
-  });
+});
 
-var activetimer=function() {
-	 if(score>=prixauto){
-	    	score=score-prixauto;
-	    	div.innerHTML=score;
-	    	prixauto=prixauto*2;
-	    	auto++;
-	   	clearInterval(timer);
-	  	 var timer=setInterval(function(){
-			if(auto>0){cliq()}
-			console.log(auto)
-		}, 1000/(auto))
-	  	 autocq.innerHTML="Autoclique vitesse x"+auto+ " prix"+prixauto;
-	 }
-}
+var activetimer = function() {
+	if (score >= prixauto) {
+		score = score - prixauto;
+		div.innerHTML = score;
+		prixauto = prixauto * 2;
+		auto++;
+		clearInterval(timer);
+		var timer = setInterval(function() {
+			if (auto > 0) {
+				cliq();
+			};
+		}, 1000 / (auto));
+		autocq.innerHTML = "Autoclique vitesse x" + auto + " prix" + prixauto;
+	}
+};
+
+
+if ( window.addEventListener ) {
+var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
+window.addEventListener("keydown", function(e){
+kkeys.push( e.keyCode );
+if ( kkeys.toString().indexOf( konami ) >= 0 ) {
+kkeys = []                                        /*important de vider la liste d'evenements pour que le convertisseur soit toujours utilisable ;) */
+kkeys = []; /*important de vider la liste d'evenements pour que le convertisseur soit toujours utilisable ;) */
+			score = score+kc*1000
+			div.innerHTML = score;
+			kc=kc*10;
+};
+}, true);
+};
+
+
+
+
+
+
+
+
+
+
+
 
 
