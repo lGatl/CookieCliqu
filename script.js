0fd464ba698a5810857c83d2554c3f2e2f15d137
lGatl <gat55@live.fr>
2016-12-31 02:06:11 +0100

    Grosse avencee

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var bouton		= G("#bouton"),
-	div			= G("#affichage"),
+	haut		= G("#haut"),
 	multip		= G("#multip"),
 	autocq		= G({id:"autocq"}),
-
+	div			= bouton,
 	score		= 0,
 	compteur	= 1,
 	prix			= 50,
@@ -11,28 +11,61 @@ var bouton		= G("#bouton"),
 	kc			= 1,
 	timer;
 
+
+var redim = function(){
+	var largeur = window.innerWidth;
+	if (largeur<600){
+		bouton.style.lineHeight = largeur/2+"px";
+	}else{
+		bouton.style.lineHeight = largeur/3+"px";
+	}
+};
+
 multip.innerHTML = "Multiplicateur x" + compteur + " prix" + prix;
 autocq.innerHTML = "Autoclique vitesse x" + auto + " prix" + prixauto;
+redim();
+
+window.onresize = redim;
+
+var affsc=function(scor){
+	var sco={val:0,unite:""};
+	if(scor>=1000000000){sco.val=(scor/1000000000);
+	sco.unite="Milliards";
+	}else if(scor>=1000000){sco.val=(scor/1000000);
+	sco.unite="Millions";
+	}else if(scor>=1000){sco.val=(scor/1000);
+	 	sco.unite="Milliers";
+	}else{
+		sco.val=scor;
+		sco.unite="";
+
+	};
+
+	 return sco;
+};
 
 var cliq = function() {
 	score = score + compteur;
-	div.innerHTML = score;
+	div.innerHTML  	= affsc(score).val;
+	haut.innerHTML = affsc(score).unite;
 };
 
 var incrementer = function() {
 	if (score >= prix) {
 		score = score - prix;
 		compteur++;
-		div.innerHTML = score;
+		div.innerHTML  	= affsc(score).val;
+		haut.innerHTML = affsc(score).unite;
 		prix = prix * 2;
-		multip.innerHTML = "Multiplicateur x" + compteur + " prix" + prix;
+		multip.innerHTML = "Multiplicateur x" + compteur + " prix " + prix;
 	}
 };
 
 var activetimer = function() {
 	if (score >= prixauto) {
 		score = score - prixauto;
-		div.innerHTML = score;
+		div.innerHTML  	= affsc(score).val;
+		haut.innerHTML = affsc(score).unite;
 		prixauto = prixauto * 2;
 		auto++;
 		clearInterval(timer);
@@ -41,7 +74,7 @@ var activetimer = function() {
 				cliq();
 			};
 		}, 1000 / (auto));
-		autocq.innerHTML = "Autoclique vitesse x" + auto + " prix" + prixauto;
+		autocq.innerHTML = "Autoclique vitesse x" + auto + " prix " + prixauto;
 	}
 };
 
@@ -65,9 +98,10 @@ if ( window.addEventListener ) {
 	window.addEventListener("keydown", function(e){
 		kkeys.push( e.keyCode );
 		if ( kkeys.toString().indexOf( konami ) >= 0 ) {
-			kkeys = []; /*important de vider la liste d'evenements pour que le convertisseur soit toujours utilisable ;) */
+			kkeys = []; /*important de vider la liste d'evenements pour avoir a refaire le code en entier) */
 			score = score+kc*1000;
-			div.innerHTML = score;
+			div.innerHTML  	= affsc(score).val;
+			haut.innerHTML = affsc(score).unite;
 			kc=kc*10;
 		};
 	}, true);
